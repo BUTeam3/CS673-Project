34b1829255f5ace1b2e0ec2e8ad270d39df58608
Joe Kutner <jpkutner@gmail.com>
2015-11-24 12:32:24 -0600

    added db config

---
+++ src/main/java/com/github/britter/springbootherokudemo/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.github.britter.springbootherokudemo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
