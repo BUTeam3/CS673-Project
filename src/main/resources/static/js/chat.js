6811661197ddc7d0ea033fa749e237dd0ba2eb46
awilner88 <awilner88@gmail.com>
2016-07-11 22:47:11 -0400

    message ids

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -33,22 +33,9 @@ $(document).on('submit', '#create_messge_form', function(){
     return false;
 });
 
-//$(function(){
-//    var mid = $('.chat_msg').length ? $('.chat_msg:last').data('message-id') : 0;
-//    $.ajax({
-//        type: "POST",
-//        url: "/chat_msg/read",
-//        beforeSend: function (xhr) {
-//            xhr.setRequestHeader('X-CSRF-Token', $('meta[name="_csrf"]').attr('content'))
-//        },
-//        data: {
-//            mid: mid,
-//        },
-//        success: function (data) {
-//            $('#chat_conversation').append(data);
-//        }
-//    });
-//});
+$(function(){
+    $('#chat_conversation').scrollTop($('#chat_conversation')[0].scrollHeight);
+});
 
 
 setInterval(function() {
