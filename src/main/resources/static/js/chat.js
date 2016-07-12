b588f89d0b10c548dafe551a3fb479b599128352
awilner88 <awilner88@gmail.com>
2016-07-11 21:01:21 -0400

    message ids

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -33,10 +33,7 @@ $(document).on('submit', '#create_messge_form', function(){
     return false;
 });
 
-
-
-
-setInterval(function() {
+$(function(){
     var mid = $('.chat_msg').length ? $('.chat_msg:last').data('message-id') : 0;
     $.ajax({
         type: "POST",
@@ -48,7 +45,27 @@ setInterval(function() {
             mid: mid,
         },
         success: function (data) {
-            $('#chat_conversation').append(data);
+            console.log(data);
+            //$('#chat_conversation').replaceWith(data);
+            //$('#chat_conversation').scrollTop($('#chat_conversation')[0].scrollHeight);
         }
     });
+});
+
+
+setInterval(function() {
+    //var mid = $('.chat_msg').length ? $('.chat_msg:last').data('message-id') : 0;
+    //$.ajax({
+    //    type: "POST",
+    //    url: "/chat_msg/read",
+    //    beforeSend: function (xhr) {
+    //        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="_csrf"]').attr('content'))
+    //    },
+    //    data: {
+    //        mid: mid,
+    //    },
+    //    success: function (data) {
+    //        $('#chat_conversation').append(data);
+    //    }
+    //});
 }, 1000);
