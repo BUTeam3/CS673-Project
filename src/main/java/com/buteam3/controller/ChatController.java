1d38b5a533157e6f56d11db6000fa91ce607ae69
awilner88 <awilner88@gmail.com>
2016-07-11 20:33:46 -0400

    message ids

--- src/main/java/com/buteam3/controller/ChatController.java
+++ src/main/java/com/buteam3/controller/ChatController.java
@@ -11,7 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.StringUtils;
 
 import com.buteam3.repository.MessageRepository;
 import com.buteam3.entity.Message;
