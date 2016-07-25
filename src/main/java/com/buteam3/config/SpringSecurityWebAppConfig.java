1b214ab56fabc466c261ce67dbcac0594b909bfc
awilner88 <awilner88@gmail.com>
2016-07-25 19:40:43 -0400

    stormpath removed requiresChannel

--- src/main/java/com/buteam3/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/buteam3/config/SpringSecurityWebAppConfig.java
@@ -10,9 +10,6 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .apply(stormpath()).and()
-            .requiresChannel()
-            .anyRequest().requiresSecure();
+        http.apply(stormpath());
     }
 }
