eef04bef51d7244ec010d219c865fc8db7ff16f4
awilner88 <awilner88@gmail.com>
2016-07-25 19:20:18 -0400

    stormpath redirect https

--- src/main/java/com/buteam3/Application.java
+++ src/main/java/com/buteam3/Application.java
@@ -26,3 +26,15 @@ public class Application {
     }
 
 }
+
+TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+    @Override
+    protected void postProcessContext(Context context) {
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern("/*");
+        securityConstraint.addCollection(collection);
+        context.addConstraint(securityConstraint);
+    }
+};
